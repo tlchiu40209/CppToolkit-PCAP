5b5120dba8177d925884224749486fe500d0e5ed
Wayne Chiu <14266510+tlchiu40209@users.noreply.github.com>
2023-10-09 11:34:04 +0200

    Massive Rewrite with Block Structure.
    Wayne - [9 Oct., 2023]
    
    1. Massive Rewrite with Block Structure.

--- waynePCAP.cpp
+++ waynePCAP.cpp
@@ -7,35 +7,106 @@
 
 #include "waynePCAP.hpp"
 
-namespace wayne {
+namespace wayne
+{
 
-PCAP::PCAP() {
-	// TODO Auto-generated constructor stub
+    namespace PCAP
+    {
+        enum blockTypes
+        {
+            sectionHeaderBlock,
+            interfaceDescriptionBlock,
+            enhancedPacketBlock,
+            simplePacketBlock,
+            nameResolutionBlock,
+            interfaceStatisticsBlock,
+            systemdJournalExportBlock,
+            decryptionSecrestsBlock,
+            customBlock
+        };
 
-}
+        class byteSeqs
+        {
+        public:
+            static char* BYTE_ORDER_BIG_ENDIAN = "\x1a\x2b\x3c\x4d";
+            static char* BYTE_ORDER_SMALL_ENDIAN = "\x4d\x3c\x2b\x1a";
+        };
 
-PCAP::~PCAP() {
-	// TODO Auto-generated destructor stub
-}
+        class identBytes
+        {
+        public:
+            static char* OPT_ENDOFOPT  = "\x00\x00";
+            static char* OPT_COMMENT = "\x01\x00";
+            static char* SHB_HARDWARE = "\x02\x00";
+            static char* SHB_OS = "\x03\x00";
+            static char* SHB_USERAPPL = "\x04\x00";
+            static char* IDB_IF_NAME = "\x02\x00";
+            static char* IDB_IF_DESCRIPTION = "\x03\x00";
+            static char* IDB_IF_IPV4ADDR = "\x04\x00";
+            static char* IDB_IF_IPV6ADDR = "\x05\x00";
+            static char* IDB_IF_MACADDR = "\x06\00";
+            static char* IDB_IF_EUIADDR = "\x07\x00";
+            static char* IDB_IF_SPEED = "\x08\x00";
+            static char* IDB_IF_TSRESOL = "\x09\x00";
+            static char* IDB_IF_TZONE = "\x0a\x00";
+            static char* IDB_IF_FILTER = "\x0b\x00";
+            static char* IDB_IF_OS = "\x0c\x00";
+            static char* IDB_IF_FCSLEN = "\x0d\x00";
+            static char* IDB_IF_TSOFFSET = "\x0e\x00";
+            static char* IDB_IF_HARDWARE = "\x0f\x00";
+            static char* IDB_IF_TXSPEED = "\x10\x00";
+            static char* IDB_IF_RXSPEED = "\x11\x00";
+            static char* EPB_EPB_FLAGS = "\x02\x00";
+            static char* EPB_EPB_HASH = "\x03\x00";
+            static char* EPB_EPB_DROPCOUNT = "\x04\x00";
+            static char* NBR_NS_DNSNAME = "\x02\x00";
+            static char* NBR_NS_DNSIP4ADDR = "\x03\x00";
+            static char* NBR_NS_DNSIP6ADDR = "\x04\x00";
+            static char* ISB_ISB_STARTTIME = "\x02\x00";
+            static char* ISB_ISB_ENDTIME = "\x03\x00";
+            static char* ISB_ISB_IFRECV = "\x04\x00";
+            static char* ISB_ISB_IFDROP = "\x05\x00";
+            static char* ISB_ISB_FILTERACCEPT = "\x06\x00";
+            static char* ISB_ISB_OSDROP = "\x07\x00";
+            static char* ISB_ISB_USRDELIV = "\x08\x00";
+        };
 
-PCAP::PCAP(const PCAP &other) {
-	// TODO Auto-generated constructor stub
+        struct block
+        {
+        private:
+            char* blockType;
+            char* blockLength;
 
-}
+        public:
+            block();
+            block()
+            virtual ~block();
+            block& operator=(const block &other);
+            block& operator=(block &other);
+            block& operator==(const block &other);
+            block& operator==(block &other);
+        };
 
-PCAP::PCAP(PCAP &&other) {
-	// TODO Auto-generated constructor stub
+        struct sectionHeaderBlock : block
+        {
 
-}
+        };
 
-PCAP& PCAP::operator=(const PCAP &other) {
-	// TODO Auto-generated method stub
+        class PCAPNG
+        {
+            public:
+                PCAPNG()
+                PCAPNG(const PCAPNG &other);
+                PCAPNG(PCAPNG &&other);
+                virtual ~PCAPNG();
+                PCAPNG& operator=(const PCAPNG &other);
+                PCAPNG& operator=(PCAPNG &other);
+                PCAPNG& operator==(const PCAPNG &other);
+                PCAPNG& operator==(PCAPNG &other);
+            private:
 
-}
 
-PCAP& PCAP::operator=(PCAP &&other) {
-	// TODO Auto-generated method stub
-
-}
+        };
+    }
 
 } /* namespace wayne */
--- waynePCAP.hpp
+++ waynePCAP.hpp
@@ -11,56 +11,21 @@
 #include <string>
 
 namespace wayne {
+    namespace PCAP
+    {
 
-struct PCAPHeaderBlock{
-private:
-	const char* blockType = 0x0A0D0D0A;
-	int totalLength;	/*Calculated*/
-	char* byteOrder;
-	int majorVersion = 1;
-	int minorVersion = 0;
-	char* sectionLength = 0xFF; /*Value must always aligned to 32 bit.*/ /*Calculated*/
-	std::map<std::string, std::string> options; // @suppress("Invalid template argument")
+        class PCAPNG
+        {
+            PCAPNG();
+            virtual ~PCAPNG();
+            PCAPNG(PCAPNG &&other);
+            PCAPNG& operator=(const PCAPNG &other);
+            PCAPNG& operator=(PCAPNG &other);
+            PCAPNG& operator==(const PCAPNG &other);
+            PCAPNG& operator==(PCAPNG &other);
+        };
 
-public:
-	/* Constructor */
-	PCAPHeaderBlock();
-	virtual ~PCAPHeaderBlock();
-	PCAPHeaderBlock(const PCAPHeaderBlock &other);
-	PCAPHeaderBlock(PCAPHeaderBlock &&other);
-	PCAPHeaderBlock operator=(const PCAPHeaderBlock &other);
-	PCAPHeaderBlock operator=(PCAPHeaderBlock &&other);
-
-	int getTotalLength(); /*Should not be changeable*/
-	char* getByteOrder();
-	int getMajorVersion();
-	int getMinorVersion();
-	char* getSectionLength(); /*Should not be changeable*/
-	std::map<std::string> getAllOptions();
-	std::string* getAllOptions();
-	char** getAllOptions();
-
-	bool setByteOrder(char* newByteOrder);
-	bool setMajorVersion(int newMajorVersion);
-	bool setMinorVersion(int newMinorVersion);
-
-
-
-};
-
-
-class PCAP {
-public:
-	PCAP();
-	virtual ~PCAP();
-	PCAP(const PCAP &other);
-	PCAP(PCAP &&other);
-	PCAP& operator=(const PCAP &other);
-	PCAP& operator=(PCAP &&other);
-private:
-
-};
-
+    }
 } /* namespace wayne */
 
 #endif /* LIB_WAYNEPCAP_HPP_ */
