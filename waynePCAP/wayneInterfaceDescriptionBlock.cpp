0bf49a48495928352691657191c021b5ce6c5917
Wayne Chiu <14266510+tlchiu40209@users.noreply.github.com>
2023-11-05 13:49:44 +0100

    Patch getOption IDB
    Wayne - [5 Nov, 2023]
    
    1. Patch getOption IDB.

--- waynePCAP/wayneInterfaceDescriptionBlock.cpp
+++ waynePCAP/wayneInterfaceDescriptionBlock.cpp
@@ -618,7 +618,92 @@ namespace wayne {
 				std::copy(this->options[option], this->options[option] + std::strlen(this->options[option]), toReturn);
 				return toReturn;
 			}
-			// 5 Nov, 2023 Here.
+			else
+			{
+				if (isOptionCurrentlyMultiple(option))
+				{
+					switch (option)
+					{
+						case optionTypes::IF_IPV4ADDR:
+						toReturn = new char[optionByteLength::IF_IPV4ADDR_LENGTH * this->multCounts[option]];
+						std::copy(this->options[option], this->options[option] + (optionByteLength::IF_IPV4ADDR_LENGTH * this->multCounts[option]), toReturn);
+						return toReturn;
+						break;
+						case optionTypes::IF_IPV6ADDR:
+						toReturn = new char[optionByteLength::IF_IPV6ADDR_LENGTH * this->multCounts[option]];
+						std::copy(this->options[option], this->options[option] + (optionByteLength::IF_IPV6ADDR_LENGTH * this->multCounts[option]), toReturn);
+						return toReturn;
+						break;
+						default:
+						return toReturn;
+						break;
+					}
+				}
+				else
+				{
+					switch (option)
+					{
+						case optionTypes::IF_IPV4ADDR:
+							toReturn = new char[optionByteLength::IF_IPV4ADDR_LENGTH];
+							std::copy(this->options[option], this->options[option] + optionByteLength::IF_IPV4ADDR_LENGTH, toReturn);
+							return toReturn;
+							break;
+						case optionTypes::IF_IPV6ADDR:
+							toReturn = new char[optionByteLength::IF_IPV6ADDR_LENGTH];
+							std::copy(this->options[option], this->options[option] + optionByteLength::IF_IPV6ADDR_LENGTH, toReturn);
+							return toReturn;
+							break;
+						case optionTypes::IF_MACADDR:
+							toReturn = new char[optionByteLength::IF_MACADDR_LENGTH];
+							std::copy(this->options[option], this->options[option] + optionByteLength::IF_MACADDR_LENGTH, toReturn);
+							return toReturn;
+							break;
+						case optionTypes::IF_EUIADDR:
+							toReturn = new char[optionByteLength::IF_EUIADDR_LENGTH];
+							std::copy(this->options[option], this->options[option] + optionByteLength::IF_EUIADDR_LENGTH, toReturn);
+							return toReturn;
+							break;
+						case optionTypes::IF_SPEED:
+							toReturn = new char[optionByteLength::IF_SPEED_LENGTH];
+							std::copy(this->options[option], this->options[option] + optionByteLength::IF_SPEED_LENGTH, toReturn);
+							return toReturn;
+							break;
+						case optionTypes::IF_TSRESOL:
+							toReturn = new char[optionByteLength::IF_TSRESOL_LENGTH];
+							std::copy(this->options[option], this->options[option] + optionByteLength::IF_TSRESOL_LENGTH, toReturn);
+							return toReturn;
+							break;
+						case optionTypes::IF_TZONE:
+							toReturn = new char[optionByteLength::IF_TZONE_LENGTH];
+							std::copy(this->options[option], this->options[option] + optionByteLength::IF_TZONE_LENGTH, toReturn);
+							return toReturn;
+							break;
+						case optionTypes::IF_FCSLEN:
+							toReturn = new char[optionByteLength::IF_FCSLEN_LENGTH];
+							std::copy(this->options[option], this->options[option] + optionByteLength::IF_FCSLEN_LENGTH, toReturn);
+							return toReturn;
+							break;
+						case optionTypes::IF_TSOFFSET:
+							toReturn = new char[optionByteLength::IF_TSOFFSET_LENGTH];
+							std::copy(this->options[option], this->options[option] + optionByteLength::IF_TSOFFSET_LENGTH, toReturn);
+							return toReturn;
+							break;
+						case optionTypes::IF_TXSPEED:
+							toReturn = new char[optionByteLength::IF_TXSPEED_LENGTH];
+							std::copy(this->options[option], this->options[option] + optionByteLength::IF_TXSPEED_LENGTH, toReturn);
+							return toReturn;
+							break;
+						case optionTypes::IF_RXSPEED:
+							toReturn = new char[optionByteLength::IF_RXSPEED_LENGTH];
+							std::copy(this->options[option], this->options[option] + optionByteLength::IF_RXSPEED_LENGTH, toReturn);
+							return toReturn;
+							break;
+						default:
+							return toReturn;
+							break;
+					}
+				}
+			}
 		}
 
 		bool interfaceDescriptionBlock::setOption(optionTypes option, const char* value, unsigned int valueLength)
